a3b423fb8b829315f1722a631b8f1de948b67340
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2021-03-02 01:47:59 +0300

    Add files via upload

---
+++ part05-Part05_05.ConstructorOverload/src/main/java/Product.java
@@ -0,0 +1,54 @@
+
+public class Product {
+
+    private String name;
+    private String location;
+    private int weight;
+    
+    public Product(String name ){
+        this.name = name;
+         this.location = "shelf";
+        this.weight = 1;
+        
+    }
+
+    public Product(String name, String location, int weight) {
+        this.name = name;
+        this.location = "shelf";
+        this.weight = 1;
+    }
+    
+    public Product(String name, String location) {
+        
+        this.name = name;
+         this.location = "field";
+        this.weight = 1;
+    }
+    
+    public Product(String name, int weight){
+        
+         this.name = name;
+         this.location = "shelf";
+        this.weight = 1000;
+        
+        
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " (" + this.weight + " kg) can be found from the " + this.location;
+    }
+}
